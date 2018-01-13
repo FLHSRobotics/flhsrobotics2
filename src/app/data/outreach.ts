75c2b8802a9c4cea5fe1678ca2cb15c4dcc15d43
theeditor <syua9708@flhs.us>
2018-01-13 13:28:22 -0500

    add outreach

--- src/app/data/outreach.ts
+++ src/app/data/outreach.ts
@@ -1,8 +1,32 @@
 export const outreach: Array<any> = [
-    {
-      title:"JHS 67 Lewis Pateur - 11/17/16",
-      subtitle:"Participating Team - FRC 3017, FTC 4785",
-      text:"This outreach was made possible by Aidan Katz who used to be part of JHS 67 FLL team.  Many of their FLL members were not very familiar with FTC and a lot of 4785 and 3017 members were also not too familiar with this year's FLL challenge.  For this reason, both teams interacted and it was a learning experience for everyone.",
-      img:"/assets/img/outreach/11-17-16.webp"
-    }
+  {
+    title: "JHS 67 Lewis Pateur - 11/17/16",
+    subtitle: "Participating Team - FRC 3017, FTC 4785",
+    text: "This outreach was made possible by Aidan Katz who used to be part of JHS 67 FLL team.  Many of their FLL members were not very familiar with FTC and a lot of 4785 and 3017 members were also not too familiar with this year's FLL challenge.  For this reason, both teams interacted and it was a learning experience for everyone.",
+    img: "/assets/img/outreach/11-17-16.webp"
+  },
+  {
+    title: "Francis Lewis Open House - 11/9/16",
+    subtitle: "Participating Teams - FRC 3017,FTC 3006,4780, 4782, 4783, 4785, 4995",
+    text: "Representing the Engineering and Robotics programs, team members stayed late to talk to prospective students and their parents.  A lot of eighth graders we spoke to were equally thrilled and intrigued by the thought of building and programming a robot.  After the conversation that the team members had with the visiting middle school students, most of them expressed interest in joining a team.",
+    img: "/assets/img/outreach/11-9-16.webp"
+  },
+  {
+    title: "Maker Faire - NY Hall of Science - 10/2/16",
+    subtitle: "Participating Teams - FRC 3017, FTC 4781, 4782, 4783, 4995",
+    text: "Members of Francis Lewis Robotics assisted in promoting FIRST programs (Junior FLL, FLL, FTC and FRC) during the 2016 NY Maker Faire held at the Hall of Science in Flushing Meadows Park.  Together with other teams from the region, our students helped by sharing their experiences with FIRST robotics to faire participants, some of whom experienced driving our robot.",
+    img: "/assets/img/outreach/10-2-16.webp"
+  },
+  {
+    title: "City of Science - York College - 9/25/16",
+    subtitle: "Participating Teams - FRC 3017, FTC 3006, 4783",
+    text: "Working with York College Astrobotics team, the Francis robotics members helped in showcasing Astrobotics' swarm robots and their NASA sponsored rover to event spectators.  Many kids were captivated by the small robot so the team made sure that young children were fully engaged during the demo. FLHS robotics members also spoke about the programming and manufacturing that goes into creating robots.  It was as much of a learning experience for our students as it was for all the kids that they interacted during the course of the day.",
+    img: "/assets/img/outreach/9-25-16.webp"
+  },
+  {
+    title: "World Science Festival Washington Square Park - 6/05/16",
+    subtitle: "Participating Teams - FRC 3017, FTC 3006, 4780, 4995",
+    text: "Just like in the previous years, Francis Lewis Robotics showed its support to NYC FIRST by helping to promote First Tech Challenge during this year's WSF.  Due inclement weather, the entire display was moved indoors to one of NYU's rooms.  Even though the weather was not cooperating, turnout was still high. Our team left exhausted but very accomplished after talking to hundreds of festival attendees.",
+    img: "/assets/img/outreach/6-5-16.webp"
+  }
 ];
