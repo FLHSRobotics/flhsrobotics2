fe8f1050a7283c6b93c945498ff0a68c7b0131ac
theeditor <syua9708@flhs.us>
2018-01-13 12:37:30 -0500

    update frc homepage

--- src/app/frc/frc.component.ts
+++ src/app/frc/frc.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-frc',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrcComponent implements OnInit {
 
-  constructor() { }
+  teamId: number;
+
+  constructor(
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.teamId = this.route.snapshot.params['teamNum'];
+    console.log(this.teamId)
   }
 
 }
--- src/app/ftc/ftc.component.ts
+++ src/app/ftc/ftc.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-ftc',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FtcComponent implements OnInit {
 
-  constructor() { }
+  teamId:Number;
+
+  constructor(
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.teamId = this.route.snapshot.params['teamNum'];
   }
 
 }
