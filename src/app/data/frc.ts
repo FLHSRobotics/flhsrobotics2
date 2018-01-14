4c435f88f67f672fd8d0ba2600e49e63d38d5fc0
theeditor <syua9708@flhs.us>
2018-01-14 09:51:48 -0500

    update frc

--- src/app/data/frc.ts
+++ src/app/data/frc.ts
@@ -1,16 +1,3 @@
 export class Frc {
-  frcTeams:Array<any> = [
-    {
-      teamId:3017,
-      description:`Founded in 2009, FRC Team 3017 (Patriots) is the oldest FIRST team at Francis Lewis High School.  Inspired by this program, the school has expanded to create eight First Tech Challenge teams and build an Engineering Program that has graduated students who pursued careers in  Engineering, Computer Science, etc.`,
-      members:[
-        {
-          name:"Someone",
-          role:"Engineering Team Captain",
-          icon:"default",
-          description:"A two year member, Jason became the manufacturing captain of the team due in part to his dedication and hard work.  Having, reached the finals at last year's NYC Regional Championship, he aspires to work even harder this year so that the team will be able to qualify for the National Championship."
-        }
-      ]
-    }
-  ];
+
 }
